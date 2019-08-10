6302809f6022d27ff07ab48fbe3093f338dafe20
yasmin <yasminefawzy22@gmail.com>
2019-08-10 18:59:52 +0200

    adding players

--- Tic Tac Toe/src/components/Board.jsx
+++ Tic Tac Toe/src/components/Board.jsx
@@ -7,19 +7,30 @@ export default class Board extends Component {
       super(props);
 
       this.state={
-        board: Array(9).fill('X')
+        board: Array(9).fill(null),
+        player: 'X'
       }
     }
 
    
-    handleClick= (event)=>{
-        console.log(event);
+    handleClick= (index)=>{
+
+      let board2 = this.state.board
+      board2[index]= this.state.player
+      let newPlayer = this.state.player === 'X' ? 'O' :'X';
+
+      this.setState({
+        board:board2,
+        player:newPlayer
+      })
+        console.log(this.state.board);
         
     }
 
   
     render() {
-     const insideBox= this.state.board.map((box,index)=><div key={index} onClick={this.handleClick}>{box}</div>)
+     const insideBox= this.state.board.map((box,index)=><div key={index} onClick={()=>
+        this.handleClick(index)}>{box}</div>)
     const status = 'Next player: X';
   
       return (
