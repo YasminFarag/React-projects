e71ae77cdb8815784e0a96217432d56c06c25498
yasmin <yasminefawzy22@gmail.com>
2019-08-10 17:25:52 +0200

    updating state

--- Tic Tac Toe/src/components/Board.jsx
+++ Tic Tac Toe/src/components/Board.jsx
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
-import {Square} from './index.js';
+//import Square from './Square';
 
-export default class Board extends React.Component {
+
+export default class Board extends Component {
     constructor(props) {
       super(props);
 
-      
+      this.state={
+        board: Array(9).fill('X')
+      }
     }
 
    
     handleClick= (event)=>{
-
+        console.log(event);
+        
     }
 
   
     render() {
-     const box= this.state.board.map((box,index)=><div className="box" key={index} onClick={this.handleClick}>{box}</div>)
-      const status = 'Next player: X';
+     const insideBox= this.state.board.map((box,index)=><div key={index} onClick={this.handleClick}>{box}</div>)
+    const status = 'Next player: X';
   
       return (
-
-
-        <div className="box">{box}</div>
+          <div>
+              <div className="status h2 text-center">{status}</div>
+        <div className="board">{insideBox}</div>
+      
+        </div>
       )
       }
     }
