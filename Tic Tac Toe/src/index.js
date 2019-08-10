6cffc520b4a999e036e908e028ea15af25c6053f
yasmin <yasminefawzy22@gmail.com>
2019-08-10 19:22:09 +0200

    index.js

--- Tic Tac Toe/src/index.js
+++ Tic Tac Toe/src/index.js
@@ -1,71 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/index.css';
+import Game from './Game';
 import registerServiceWorker from './registerServiceWorker';
 
-import './styles/index.css';
+
+
 
 
 // ==========================
 // Components
 // ==========================
-export const Square =(props)=>{
- 
-    return (
-      <button className="square" onClick ={props.onClick}>
-        {props.value}
-      </button>
-    );
-  }
 
+// class Board extends React.Component {
+//   renderSquare(i) {
+//     return <Square />;
+//   }
 
+//   render() {
+//     const status = 'Next player: X';
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
+//     return (
+//       <React.Fragment>
+//         <h1>hhhhh</h1>
+//         <div className="status h2 text-center">{status}</div>
+//         <div className="board">
+//             {this.renderSquare(0)}
+//             {this.renderSquare(1)}
+//             {this.renderSquare(2)}
+//             {this.renderSquare(3)}
+//             {this.renderSquare(4)}
+//             {this.renderSquare(5)}
+//             {this.renderSquare(6)}
+//             {this.renderSquare(7)}
+//             {this.renderSquare(8)}
+//         </div>
+//       </React.Fragment>
+//     );
+//   }
+// } 
 
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <React.Fragment>
-        <div className="status h2 text-center">{status}</div>
-        <div className="board">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-        </div>
-      </React.Fragment>
-    );
-  }
-} 
-
-class Game extends React.Component {
-  render() {
-    return (
-      <article className="game container mt-5">
-        <section className="row">
-          <div className="col-sm-8 game-board">
-            <Board />
-          </div>
-          <div className="col-sm-4 game-info">
-            <p className="h2">{/* status */}</p>
-            <ul className="nav nav-pills flex-column">
-              {/* TODO */}
-            </ul>
-          </div>
-        </section>
-      </article>
-    );
-  }
-}
 
 // ========================================
 
