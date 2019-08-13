df19b4948b7a2ae0a546838b687c6f7f93a5fbfa
yasmin <yasminefawzy22@gmail.com>
2019-08-13 11:42:15 +0200

    passing Square component to the Board to creat restart function

--- Tic Tac Toe/src/components/Square.jsx
+++ Tic Tac Toe/src/components/Square.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 
 export default class Square extends Component {
+
+    
+
+    handleSubmit(event){
+        event.preventDefault();
+        console.log(event.target.value);
+       // const submit =this.props.submit
+       console.log(this.props.player)
+        
+    }
+    
+   
     render() {
         return (
             <div>
-                <h2>check player</h2>
-                <form>
+                <h2>Choose player</h2>
+                <form onSubmit={this.handleSubmit}>
                    <label>PLayer X</label>
                    <input type="radio" />
                    {/* player O */}
                    <label>PLayer O</label>
                    <input type="radio" />
                     </form>
+
+                    <button onClick={this.props.reset}>Restart </button>
 
                     </div>
         )
