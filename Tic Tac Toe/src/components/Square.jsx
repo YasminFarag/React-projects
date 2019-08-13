4f2315efd8da00167c983f1e4c18ca3b459e9feb
yasmin <yasminefawzy22@gmail.com>
2019-08-13 14:27:11 +0200

    styling

--- Tic Tac Toe/src/components/Square.jsx
+++ Tic Tac Toe/src/components/Square.jsx
@@ -1,31 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
 
 export default class Square extends Component {
 
     
-
+/* 
     handleSubmit(event){
         event.preventDefault();
         console.log(event.target.value);
        // const submit =this.props.submit
-       console.log(this.props.player)
+       //console.log(this.props.player)
         
     }
-    
+     */
    
     render() {
         return (
+            
             <div>
-                <h2>Choose player</h2>
+                 {/* <h2>Choose player</h2>
+
                 <form onSubmit={this.handleSubmit}>
                    <label>PLayer X</label>
                    <input type="radio" />
-                   {/* player O */}
-                   <label>PLayer O</label>
+                  
+                    <label>PLayer O</label>
                    <input type="radio" />
-                    </form>
+                    </form>   */}
 
-                    <button onClick={this.props.reset}>Restart </button>
+                    <button className='resetbtn' onClick={this.props.reset}>Reset </button>
 
                     </div>
         )
