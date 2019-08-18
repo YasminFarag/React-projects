8d12f0306ac46a9f81466843934d9c39cd79fddb
yasmin <yasminefawzy22@gmail.com>
2019-08-18 18:01:52 +0200

    add Route tage

--- blog-yasmin/src/App.js
+++ blog-yasmin/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
-import {NativeRouter as Router, Link, Route, Switch} from 'react-router-native';
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import CreateAPost from './components/CreateAPost';
+import ShowCurrentPosts from './components/ShowCurrentPosts';
 
 
 export default function App() {
   return (
     <Router>
     <section>
-
+      <p>hello</p>
+      <nav>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to ="CreateAPost">Create A Post</Link></li>
+        <li><Link to="ShowCurrentPosts">Show Current Posts</Link></li>
+      </nav>
+      <Switch>
+        <Route exact path="/" components={Home} />
+        <Route path="CreateAPost" components={CreateAPost} />
+        <ShowCurrentPosts path="ShowCurrentPosts" components= {ShowCurrentPosts} />
+        </Switch>
       
     </section>
     </Router>
