2934cb9ee6c6708c69c1a3aa9a457fd0fd980777
yasmin <yasminefawzy22@gmail.com>
2019-08-18 16:40:05 +0200

    create links

--- blog-yasmin/src/components/Home.jsx
+++ blog-yasmin/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export class CreateAPost extends Component {
+export class Home extends Component {
     render() {
         return (
             <div>
@@ -10,4 +10,4 @@ export class CreateAPost extends Component {
     }
 }
 
-export default CreateAPost
+export default Home
