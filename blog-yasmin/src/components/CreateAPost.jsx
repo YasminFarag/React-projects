5fbcb9949a2b9716e76b04c19b37d5d39d611fa5
yasmin <yasminefawzy22@gmail.com>
2019-08-21 21:41:43 +0200

    set the form

--- blog-yasmin/src/components/CreateAPost.jsx
+++ blog-yasmin/src/components/CreateAPost.jsx
@@ -18,24 +18,15 @@ class CreateAPost extends Component {
        
           <label>
             UserName:
-            <input
-              type="text"
-              ref="txt"
-              /*  text={this.state.text} */
-              
-            />
+            <input  type="text" ref="txt" /*  text={this.state.text} */ />  
           </label>
           <br />
 
           <br />
           <label>
             Title:
-            <input
-              type="text"
-              ref="tit"
-              /* title={this.state.title} */
-              
-            />
+            <input type="text" ref="tit" /* title={this.state.title} */ />
+          
           </label>
           <br />
 
@@ -43,15 +34,9 @@ class CreateAPost extends Component {
 
           <label className="box">
             Content:
-            <textarea
-              type="text"
-              ref="con"
-              /* content={this.state.content} */
-              
-            />
+            <textarea type="text" ref="con" /* content={this.state.content} */ />
           </label>
           <br />
-
           <button type="button" onClick={()=> this.props.handleSubmit(this.refs.txt.value,this.refs.tit.value,this.refs.con.value)}>
             Creat Post
           </button>
